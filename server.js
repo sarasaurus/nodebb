b97bc99b9db7d4cc0e801005f42270f37d605cd7
sarasaurus <bixler.s.e@gmail.com>
2018-04-14 10:30:23 -0700

    block pt 2

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ function start(route, handle) {
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
 
-        route(handle, pathname);
+        route(handle, pathname, response);
 
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World");
-        response.end();
+        // response.writeHead(200, {"Content-Type": "text/plain"});
+        // response.write("Hello World");
+        // response.end();
     }
     
     http.createServer(onRequest).listen(8888);
