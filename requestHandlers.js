a93be27892a9f7509eaa243a6f2464fe62b06760
sarasaurus <bixler.s.e@gmail.com>
2018-04-14 10:19:07 -0700

    blocking pt 1

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,20 @@
 function start() {
     console.log("request handler 'start' was called");
+    
+    function sleep (milliSeconds) {
+        var startTime = new Date().getTime();
+        while (new Date().getTime() < startTime + milliSeconds);
+    }
+
+    sleep(10000);
+    return "Hello Start";
 }
 
+
 function upload() {
-    cosole.log("request handler 'upload' was called");
+    console.log("request handler 'upload' was called");
+    return "Hello Upload";
 }
+
 exports.start = start;
 exports.upload = upload;
