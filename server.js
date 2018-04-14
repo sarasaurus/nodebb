29c09aa59d8594dc119ae9237ee7567e94cb9d0c
sarasaurus <bixler.s.e@gmail.com>
2018-04-13 20:43:12 -0700

    added router

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 
 var http = require("http");
+var url = require("url");
 
-function onRequest(request, response){
-    console.log("Request received.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-}
+function start(route) {
+    function onRequest(request, response){
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
 
-http.createServer(onRequest).listen(8888);
-console.log("Server has started.");
+        route(pathname);
+
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+    
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started.");
+};
+
+exports.start = start;
+
 
