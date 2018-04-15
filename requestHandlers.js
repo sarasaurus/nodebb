1bf4f8acd97dc4cd3d2e2c6a77daa959722e554d
sarasaurus <bixler.s.e@gmail.com>
2018-04-15 11:19:10 -0700

    non-blocking proof

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,11 +4,20 @@ var exec = require("child_process").exec;
 function start(response) {
     console.log("request handler 'start' was called");
 
-    exec("ls -lah", function (error, stdout, stderr) {
-           response.writeHead(200, {"Content-Type": "text/plain"});
-           response.write(stdout);
-           response.end();
-        });
+    // exec("ls -lah", function (error, stdout, stderr) {
+    //        response.writeHead(200, {"Content-Type": "text/plain"});
+    //        response.write(stdout);
+    //        response.end();
+    //     });
+
+    //expensive operation
+    exec ("find /",
+    { timeout: 10000, maxBuffer: 20000*1024 },
+    function (error, stdout, stderr) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(stdout);
+        response.end();
+    });
 }
 
 
